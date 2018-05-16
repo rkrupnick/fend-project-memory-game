9f4eaffe77bcfb4ba4231635ad0526d81541e7aa
Rachel Krupnick <rachel@rachelkrupnick.com>
2018-05-16 15:40:22 -0400

    Add event listener to deck

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 /*
- * Create a list that holds all of your cards
+ * Create a list that holds all of your card images
  */
 const cardImages = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
   "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf",
   "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
+/*
+* Create array from card deck in HTML
+*/
 const deck = document.querySelectorAll('.card');
 const cards = Array.from(deck);
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -45,3 +49,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ document.getElementById("deck").addEventListener("click", function(e) {
+  if(e.target && e.target.nodeName == "LI") {
+    console.log("item clicked!");
+  }
+ });
