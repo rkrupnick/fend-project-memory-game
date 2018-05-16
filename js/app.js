66fe976f1f1a203304c10ea80025390d531afb80
Rachel Krupnick <rachel@rachelkrupnick.com>
2018-05-16 15:07:29 -0400

    Move card classes from HTML to JS to form card array

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+const cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
+  "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf",
+  "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
