c6273f33ddf0d4a8aa94511bd5b744bd195a6242
Rachel Krupnick <rachel@rachelkrupnick.com>
2018-05-17 14:46:17 -0400

    FEAT: Add option to start again in modal

--- js/app.js
+++ js/app.js
@@ -7,13 +7,14 @@ const standardImages = ["fa-gem", "fa-gem", "fa-paper-plane", "fa-paper-plane",
   "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf",
   "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
-const starWarsImages = ["fa-mandalorian", "fa-mandalorian", "fa-old-republic", "fa-old-republic",
-  "fa-rebel", "fa-rebel", "fa-sith", "fa-sith", "fa-galactic-senate", "fa-galactic-senate", "fa-galactic-republic",
-  "fa-galactic-republic", "fa-empire", "fa-empire", "fa-first-order", "fa-first-order"];
+const starWarsImages = ["fa-mandalorian", "fa-mandalorian", "fa-old-republic",
+  "fa-old-republic", "fa-rebel", "fa-rebel", "fa-sith", "fa-sith", "fa-galactic-senate",
+  "fa-galactic-senate", "fa-galactic-republic", "fa-galactic-republic", "fa-empire",
+  "fa-empire", "fa-first-order", "fa-first-order"];
 
 const transportationImages = ["fa-ambulance", "fa-ambulance", "fa-bus", "fa-bus",
-  "fa-car", "fa-car", "fa-fighter-jet", "fa-fighter-jet", "fa-helicopter", "fa-helicopter", "fa-space-shuttle",
-  "fa-space-shuttle", "fa-subway", "fa-subway", "fa-taxi", "fa-taxi"];
+  "fa-car", "fa-car", "fa-fighter-jet", "fa-fighter-jet", "fa-helicopter", "fa-helicopter",
+  "fa-space-shuttle", "fa-space-shuttle", "fa-subway", "fa-subway", "fa-taxi", "fa-taxi"];
 /*
 * Create array from card deck in HTML
 */
@@ -26,6 +27,8 @@ const choices = document.querySelector("#choices");
 const close = document.querySelector(".close");
 const modal = document.getElementById("modal");
 const endGame = document.querySelector(".end-game");
+const no = document.querySelector(".no");
+const yes = document.querySelector(".yes");
 
 let openCards = [];
 let moveCounter = 0;
@@ -53,17 +56,17 @@ function addImagestoCards() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
     }
 
-    return array;
+  return array;
 }
 
 
@@ -79,17 +82,17 @@ function shuffle(array) {
  */
 
 
- function showImage(e) {
+function showImage(e) {
   e.target.classList.add("open", "show"); // Add open and show class to clicked card
- }
+}
 
- function openCardsMatch() {
+function openCardsMatch() {
   if (openCards.length === 2) {
     if (openCards[0].children[0].classList.value === openCards[1].children[0].classList.value) {
       lockMatchedCards(openCards);
       matchCounter += 1;
     }  else {
-    setTimeout(function() {
+      setTimeout(function() {
       hideUnmatchedCards();
     }, 750)
   }
@@ -104,20 +107,19 @@ function checkWin() {
   }
 }
 
- function lockMatchedCards() {
+function lockMatchedCards() {
   openCards[0].classList.add("match");
   openCards[1].classList.add("match");
   openCards = [];
+}
 
- }
-
- function hideUnmatchedCards() {
+function hideUnmatchedCards() {
   openCards[0].classList.remove("open", "show");
   openCards[1].classList.remove("open", "show");
   openCards = [];
- }
+}
 
- function updateMoves() {
+function updateMoves() {
   moveCounter += 1;
   moves.innerText = moveCounter;
   if (moveCounter === 21) {
@@ -128,27 +130,27 @@ function checkWin() {
     numberStars = 1;
     removeStar(1);
   }
- }
+}
 
- function removeStar(star) {
+function removeStar(star) {
   stars.children[star].children[0].classList.remove("fa");
   stars.children[star].children[0].classList.add("far");
- }
+}
 
- function resetStars() {
+function resetStars() {
   stars.children[0].children[0].classList.remove("far");
   stars.children[0].children[0].classList.add("fa");
   stars.children[1].children[0].classList.remove("far");
   stars.children[1].children[0].classList.add("fa");
- }
+}
 
- function startAgain() {
+function startAgain() {
   for (let i=0; i<cards.length; i++) {
     if (cardImages === starWarsImages) {
       cards[i].children[0].classList = "fab";
     } else {
     cards[i].children[0].classList = "fa";
-  }
+    }
     cards[i].classList = "card";
   }
   moveCounter = 0;
@@ -156,20 +158,20 @@ function checkWin() {
   matchCounter = 0;
   resetStars();
   startGame();
- }
+}
 
- document.getElementById("deck").addEventListener("click", function(e) {
+document.getElementById("deck").addEventListener("click", function(e) {
   if(e.target && e.target.nodeName == "LI") {
     showImage(e);
     openCards.unshift(e.target);    // Add clicked card to openCards array
     updateMoves();
     openCardsMatch(); // Check if cards Match
   }
- });
+});
 
- restart.addEventListener("click", function() {
+restart.addEventListener("click", function() {
   startAgain();
- })
+})
 
 choices.addEventListener("click", function(e) {
   if(e.target && e.target.nodeName == "LI") {
@@ -185,9 +187,18 @@ choices.addEventListener("click", function(e) {
       startAgain();
     }
   }
- });
+});
 
 // To close the modal
 close.addEventListener("click", function() {
   modal.style.display = "none";
+})
+
+no.addEventListener("click", function() {
+  modal.style.display = "none";
+})
+
+// To restart from modal
+yes.addEventListener("click", function() {
+  startAgain();
 })
